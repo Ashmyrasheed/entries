773a68db1fa7a62077aedbd2ff68a48cbb1fdc87
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-04 11:15:58 +0530

    first

---
+++ entries.py
@@ -0,0 +1,31 @@
+import requests
+
+import json
+
+import mysql.connector
+
+
+
+try:
+
+    mydb = mysql.connector.connect(host = 'localhost', user = 'root',password = '',database='publicapidb')
+
+except mysql.connector.Error as e:
+
+    print("Connector error ",e)
+
+mycursor = mydb.cursor()
+
+data = requests.get("https://api.publicapis.org/entries").text
+
+data_info = json.loads(data)
+for i in data_info['entries']:
+    
+    try:
+        sql = "INSERT INTO `publicapidata`(`api`,`description`,`auth`,`https`,`cors`,`link`,`category`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        data = (i['API'],i['Description'],i['Auth'],i['HTTPS'],i['Cors'],i['Link'],i['Category'])
+        mycursor.execute(sql,data)
+        mydb.commit()
+    except mysql.connector.Error as e:
+        print("Error ",e)
+    print("data inserted succesfully, ", i['API'])
